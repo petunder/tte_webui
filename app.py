0b6e5af6c7db00eba1c60f55021b5998fdcbbb20
mashutiks <79470494+mashutiks@users.noreply.github.com>
2024-07-30 17:23:44 +0300

    Update app.py
    app code changed in accord with union

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 # app.py
 import gradio as gr
-from ui.audio_interface import create_audio_interface
-from ui.transcription_interface import create_transcription_interface
+from ui.audio_interface import create_combined_interface
 from ui.settings_interface import create_settings_interface
 from ui.text2voice_interface import create_text2voice_interface  # Добавим новый импорт
 
@@ -11,21 +10,18 @@ def main():
         
         with gr.Tabs() as tabs:
             with gr.TabItem("Audio Processing", id="audio_tab"):
-                audio_interface = create_audio_interface()
-            with gr.TabItem("Audio Transcription", id="transcription_tab"):
-                transcription_interface = create_transcription_interface()
-#            with gr.TabItem("Text to Voice", id="text2voice_tab"):  # Добавим новую вкладку
-#                text2voice_interface = create_text2voice_interface()
+                audio_interface = create_combined_interface()
+            
+            # with gr.TabItem("Text to Voice", id="text2voice_tab"):  # Добавим новую вкладку
+            #     text2voice_interface = create_text2voice_interface()
             with gr.TabItem("Settings", id="settings_tab"):
                 settings_interface = create_settings_interface()
 
         def on_tab_select(tab: gr.SelectData):
             if tab.value == "Audio Processing":
                 return audio_interface.update()
-            elif tab.value == "Transcription":
-                return transcription_interface.update()
-#            elif tab.value == "Text to Voice":
-#                return text2voice_interface.update()
+            # elif tab.value == "Text to Voice":
+            #     return text2voice_interface.update()
             return None
 
         tabs.select(on_tab_select, None, None)
