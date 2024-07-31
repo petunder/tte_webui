1bf13339cb7ba1b827cecd84b2a7ba363ab05caf
mashutiks <79470494+mashutiks@users.noreply.github.com>
2024-08-01 00:39:04 +0300

    Add files via upload
    created the processor of txt2img

---
+++ modules/txt2img_processor.py
@@ -0,0 +1,18 @@
+# text2image_technology.py
+from classes.txt2img import Text2ImageProcessor
+
+def generate_images(prompt, negative_prompt, num_inference_steps, guidance_scale, num_images, width, height, image_format):
+    processor = Text2ImageProcessor()
+    image_paths = []
+    for _ in range(num_images):
+        image_path = processor.generate_image(
+            prompt=prompt,
+            negative_prompt=negative_prompt,
+            num_inference_steps=num_inference_steps,
+            guidance_scale=guidance_scale,
+            width=width,
+            height=height,
+            image_format=image_format
+        )
+        image_paths.append(image_path)
+    return image_paths
