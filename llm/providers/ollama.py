a7f6f2c4a059b5e9591cc4debdd30790c65999c8
mashutiks <79470494+mashutiks@users.noreply.github.com>
2024-08-03 23:59:50 +0300

    Update ollama.py
    enabled getting ollama url from settings

--- llm/providers/ollama.py
+++ llm/providers/ollama.py
@@ -1,17 +1,18 @@
 import requests
 import logging
-from configparser import ConfigParser
+#from configparser import ConfigParser
+from classes.settings import Settings  # Импорт класса Settings
 
 logger = logging.getLogger(__name__)
-# Read configuration
-config = ConfigParser()
-config.read('config.ini')
-ollama_url = config.get('ollama', 'url')
+
+# Создайте экземпляр класса Settings
+settings = Settings()
+# Извлеките URL из настроек
+ollama_url = settings.get_setting('ollama_url')
 
 def process_chunk(chunk, model, system_prompt=None, temperature=0.3, top_k=40, top_p=0.9, repeat_penalty=1.2, max_tokens=2048):
     logger.debug(f"Sending request to Ollama API. Chunk length: {len(chunk)}")
-   
-    
+
     payload = {
         "model": model,
         "prompt": f"Please edit this text, correcting any errors and improving its clarity and coherence:\n\n{chunk}",
@@ -24,12 +25,12 @@ def process_chunk(chunk, model, system_prompt=None, temperature=0.3, top_k=40, t
             "num_predict": max_tokens
         }
     }
-    
+
     if system_prompt:
         payload["system"] = system_prompt
-    
+
     headers = {'Content-Type': 'application/json'}
-    
+
     try:
         logger.debug("Sending POST request to Ollama API")
         response = requests.post(ollama_url, headers=headers, json=payload, timeout=90)
